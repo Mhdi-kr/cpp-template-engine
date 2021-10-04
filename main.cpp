21adbbfc3dfa18d195a341987c1e207e4162c083
mhdi-kr <mhdi_kr@outlook.com>
2021-10-04 19:35:51 +0330

    feat: main.cpp init with TODOs

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Take string document object model as input
+    string input = "<p>\"{{ console.log(1+1) }}\"</p>";
+    // TODO Parse DOM string and return a traversable DOM tree
+    // TODO Traverse DOM tree using(post order traversal)[] Check if innerHtml of each node has handle bars
+    // TODO Evaluate handlebars using a emendable Javascript engine
+    // TODO Replace handlebars content with static string
+    // TODO Stringify DOM tree
+    // TODO Return HTML string file
+}
